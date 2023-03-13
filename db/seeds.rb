fea91d19c9358e7a8be4f23f85c99cc243e229fa
Gustavo Re <gustavo.re@inforoute.com.br>
2023-03-13 18:10:32 -0300

    Feat: update comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create(email: "jhon@doe.com", password: "password", password_confirmation: "password")
+User.create(email: "john@doe.com", name: "John" ,password: "password", password_confirmation: "password")
 
 10.times do |x|
   Post.create(title: "Title #{x}", body: "this is #{x} body here!", user_id: User.first.id)
